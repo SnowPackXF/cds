4dbde0637fb94ce67d4beb5cc39a1e28ee60ece1
guixuefeng <331603101@qq.com>
2019-07-16 13:26:09 +0800

    Frist commit all.

---
+++ src/main/java/com/bfgy/cds/service/CdsTestUserService.java
@@ -0,0 +1,12 @@
+package com.bfgy.cds.service;
+
+import com.bfgy.cds.entity.TestUser;
+
+/**
+ * @Author gxf
+ * @Date 2019/7/15
+ */
+
+public interface CdsTestUserService {
+    void add(TestUser user);
+}
